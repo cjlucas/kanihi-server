4fc4f583172180fb309f9f3f3adb5fa8f8d4ee52
Chris Lucas <chris@chrisjlucas.com>
2013-06-20 13:26:13 -0700

    added track deletion support to iTunes parser job; added job logging

--- app/jobs/base_job.rb
+++ app/jobs/base_job.rb
@@ -1,11 +1,16 @@
 class BaseJob
+  # start delayed_job hooks
+  
   def enqueue(job)
   end
 
   def before(job)
+    @logger = get_logger(job.id)
+    log_header(job)
   end
 
   def after(job)
+    logger.close
   end
 
   def success(job)
@@ -22,6 +27,34 @@ class BaseJob
 
   def max_attempts
     1
+  end
+
+  # end delayed_job hooks
+ 
+  def log_header(job)
+    logger.info('starting job:')
+    logger.info(job.handler)
+  end
+
+  def logger
+    @logger
+  end
+
+  def get_logger(job_id)
+    logger = Logger.new(log_path(job_id))
+    logger.level = $DEBUG ? Logger::DEBUG : Logger::INFO
+    logger.formatter = proc do |severity, datetime, progname, msg|
+      "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}] #{severity} - #{msg}\n" 
+    end
+
+    logger
+  end
+
+  def log_path(job_id)
+    log_dir = Rails.root.join('log')
+    Dir.mkdir(log_dir) unless Dir.exists?(log_dir)
+
+    log_dir.join("job-#{job_id}.log")
   end
 
   def update_source
--- app/jobs/itunes_parser_job.rb
+++ app/jobs/itunes_parser_job.rb
@@ -10,35 +10,63 @@ class ITunesLibraryParserJob < BaseJob
   # start delayed_job hooks
   
   def perform
-    ITunesLibrary.parse(@src.location) do |track_info| 
-      handle_track_info(track_info)
+    uris = get_uris
+    # remove tracks from database that are no longer in iTunes library
+    @src.tracks.all.each do |track|
+      next if uris.include?(URI::File.new_with_uri_string(track.uri))
+      logger.info("Deleting #{track.uri} from database")
+      track.destroy
     end
+
+    # add/update tracks
+    uris.each { |uri| handle_uri(uri) }
 
     update_source
   end
 
   # end delayed_job hooks
-  
-  def build_track_info_normalized
+ 
+  def get_uris
+    uris = []
+    ITunesLibrary.parse(@src.location) do |track_info|
+      track_info_normalized = normalize_track_info(track_info)
+      
+      if track_info_normalized.has_key?(:location)
+        itunes_uri = URI(track_info_normalized[:location])
+        fpath = URI.unescape(itunes_uri.path)
+        uris << URI::File.new_with_path(fpath)
+      end
+    end
+
+    uris
+  end 
+
+  def normalize_track_info(track_info)
     track_info_normalized = {}
-    @track_info.each do |key, value|
+    track_info.each do |key, value|
       track_info_normalized[self.class.normalize(key)] = value
     end
 
     track_info_normalized
   end
 
-  def handle_track_info(track_info)
-    @track_info = track_info
-    @track_info_normalized = build_track_info_normalized
-    
-    if @track_info_normalized.has_key?(:location)
-      uri = URI(@track_info_normalized[:location])
-      fpath = URI.unescape(uri.path)
-      if File.exists?(fpath)
-        t = Track.track_for_file_path(fpath)
+  def handle_uri(uri)
+    if uri.exists?
+      t = Track.track_for_file_path(URI.unescape(uri.path))
+      unless t.sources.include?(@src)
+        t.sources << @src
         t.save
       end
+      
+      now = Time.now
+      if now - t.created_at < 5
+        logger.info("Added #{uri.path}")
+      elsif now - t.updated_at < 5
+        logger.info("Updated #{uri.path}")
+      end
+
+    else
+      logger.info("#{uri.path} doesn't exist. Skipping.")
     end
   end
 
