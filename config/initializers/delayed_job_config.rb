e82fd2f8f1358d404f54b958224a95edc826a834
Chris Lucas <chris@chrisjlucas.com>
2013-07-04 15:34:21 -0700

    [delayed_job] decreased sleep_delay

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,5 @@
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 10
+Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 3
 #Delayed::Worker.max_run_time = 10.minutes
 Delayed::Worker.read_ahead = 10
